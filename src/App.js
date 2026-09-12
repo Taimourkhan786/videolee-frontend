3d3a0b44e2732806bb36205d000198f38d7e640e
Taimourkhan786 <taimourkhan836@gmail.com>
2026-09-12 14:43:45 +0500

    fix netlify deployment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://videolee-backend.onrender.com");
 
 function App() {
   const localVideo = useRef(null);
